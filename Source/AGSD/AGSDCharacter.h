706803664fe5cfa3dfdc92e8d71b11d1507eb34b
Never-Sleep-City <dek1423@naver.com>
2024-11-18 14:45:41 +0900

    Backup
    저장용

--- Source/AGSD/AGSDCharacter.h
+++ Source/AGSD/AGSDCharacter.h
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 // Copyright Epic Games, Inc. All Rights Reserved.
 
 #pragma once
@@ -153,3 +154,177 @@ public:
 	TSubclassOf<AProjectile_Beta> CurrentProjectile;
 };
 
+=======
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "Logging/LogMacros.h"
+#include "Projectile_A.h"
+#include "Projectile_Beta.h"
+#include "WeaponDataTable.h"
+
+#include "Blueprint/UserWidget.h"
+
+#include "AGSDCharacter.generated.h"
+
+class USpringArmComponent;
+class UCameraComponent;
+class UInputMappingContext;
+class UInputAction;
+struct FInputActionValue;
+
+DECLARE_LOG_CATEGORY_EXTERN(LogTemplateCharacter, Log, All);
+
+UCLASS(config=Game)
+class AAGSDCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+	/** Camera boom positioning the camera behind the character */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	USpringArmComponent* CameraBoom;
+
+	/** Follow camera */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	UCameraComponent* FollowCamera;
+	
+	/** MappingContext */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	UInputMappingContext* DefaultMappingContext;
+
+	/** Jump Input Action */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	UInputAction* JumpAction;
+
+	/** Move Input Action */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	UInputAction* MoveAction;
+
+	/** Look Input Action */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	UInputAction* LookAction;
+
+	/** Dash Input Action */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	UInputAction* DashAction;
+
+public:
+	AAGSDCharacter();
+	virtual void Tick(float DeltaTime) override;
+
+	FVector CharacterLocation;//캐릭터 위치
+
+	FVector TraceHitLocation;  // 라인트레이스 충돌 위치
+	FVector TraceHitDirection; // 라인트레이스 충돌 방향
+protected:
+
+	/** Called for movement input */
+	void Move(const FInputActionValue& Value);
+
+	/** Called for looking input */
+	void Look(const FInputActionValue& Value);
+
+	void Dash();
+		
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Movement")
+	UCharacterMovementComponent* CharacterMovementComponent;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Projectile")
+	TSubclassOf<class AProjectile_Beta> ProjectileClass;
+
+protected:
+	// APawn interface
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	
+	// To add mapping context
+	virtual void BeginPlay();
+
+	UPROPERTY(EditAnywhere, Category = "UI")
+	TSubclassOf<UUserWidget> HealthBarUIBPClass;
+
+	UPROPERTY()
+	UUserWidget* HealthBarWidget;
+
+public:
+	/** Returns CameraBoom subobject **/
+	FORCEINLINE class USpringArmComponent* GetCameraBoom() const { return CameraBoom; }
+	/** Returns FollowCamera subobject **/
+	FORCEINLINE class UCameraComponent* GetFollowCamera() const { return FollowCamera; }
+
+
+public:
+	//캐릭터 스탯
+	int32 MaxHealth;
+	int32 CurrentHealth;
+	int32 Defense;
+
+	int32 CharacterLevel;   // 캐릭터 레벨
+	int32 CurrentXP;        // 현재 경험치
+	int32 XPToNextLevel;    // 다음 레벨까지 필요한 경험치
+
+	UFUNCTION(BlueprintCallable, Category = "UI")
+	void UpdateHealthBar(); //체력바 갱신함수
+	UFUNCTION(BlueprintCallable, Category = "UI")
+	void UpdateXPBar(); //경험치바 갱신함수
+
+	// XP를 추가하고 레벨 업을 처리하는 함수
+	void AddXP(int32 XPAmount);
+	void LevelUp();
+
+	//무기 발사
+	UFUNCTION()
+	void Fire();
+
+	UFUNCTION()
+	void StartFiring();
+
+	UFUNCTION()
+	void StopFiring();
+
+	//무기 공격속도
+	float FireRate;
+
+	//무기 타이머
+	FTimerHandle FireTimerHandle;
+	FTimerHandle FireRateTimerHandle;
+
+	//무기 탄환 숫자
+	int Numberofprojectile;
+
+	//탄환 각도
+	float SpreadAngle;
+
+	//무기 교체
+	UFUNCTION()
+	void WeaponSwap();
+
+	UPROPERTY(VisibleDefaultsOnly, Category = Mesh)
+	USkeletalMeshComponent* FPSMesh;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Gameplay)
+	FVector MuzzleOffset;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Gameplay)
+	FVector MuzzleLocation;
+
+	UPROPERTY()
+	FString WeaponID;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Data")
+	UDataTable* WeaponDataTable;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Data")
+	UDataTable* WeaponDataTableRef;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Projectile")
+	TSubclassOf<AProjectile_Beta> CurrentProjectile;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	UStaticMeshComponent* CurrentWeaponMesh;
+
+};
+
+>>>>>>> Stashed changes
