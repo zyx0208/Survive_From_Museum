c1d45205d71324a61b4415e5e0c57c3566a880a7
doyeon-SM <82075191+doyeon-SM@users.noreply.github.com>
2025-03-07 18:02:36 +0900

    03.07.02
    결과창 UI 제작
    획득 악세서리 출력
    스코어 출력은 보류(점수측정방식 고민)

--- Source/AGSD/ResultUI.cpp
+++ Source/AGSD/ResultUI.cpp
@@ -2,4 +2,58 @@
 
 
 #include "ResultUI.h"
+#include "AGSDGameMode.h"
+#include "Kismet/GameplayStatics.h"
 
+void UResultUI::NativeConstruct()
+{
+    Super::NativeConstruct();
+
+    if (CloseResultButton)
+    {
+        CloseResultButton->OnClicked.AddDynamic(this, &UResultUI::CloseResult);
+    }
+    PopulateAccessoryIcons();
+}
+
+void UResultUI::CloseResult()
+{
+    AAGSDGameMode* Gamemode = GetWorld() ? Cast<AAGSDGameMode>(GetWorld()->GetAuthGameMode<AAGSDGameMode>()) : nullptr;
+    if (Gamemode)
+    {
+        Gamemode->ResetAccessoryData();
+    }
+    else
+    {
+        UE_LOG(LogTemp, Warning, TEXT("GameMode 찾을 수 없음"));
+    }
+}
+
+void UResultUI::PopulateAccessoryIcons()
+{
+    if (!AccessoryWrapBox && !AccessoryDataTable) return;
+    AccessoryWrapBox->ClearChildren();
+
+    static const FString ContextString(TEXT("PopulateAccessotyIcons"));
+    TArray<FName> RowNames = AccessoryDataTable->GetRowNames();
+
+    for (FName RowName : RowNames)
+    {
+        FAccessoryData* Accessory = AccessoryDataTable->FindRow<FAccessoryData>(RowName, ContextString, true);
+        if (Accessory && Accessory->bIsAcquired && Accessory->AccessoryIcon)
+        {
+            // 새로운 UImage 위젯 생성
+            UImage* NewAccessoryImage = NewObject<UImage>(this);
+            if (NewAccessoryImage)
+            {
+                NewAccessoryImage->SetBrushFromTexture(Accessory->AccessoryIcon);
+                NewAccessoryImage->SetVisibility(ESlateVisibility::Visible);
+                NewAccessoryImage->SetDesiredSizeOverride(FVector2D(332, 332)); // 아이콘 크기 설정
+
+                // WrapBox에 추가
+                AccessoryWrapBox->AddChildToWrapBox(NewAccessoryImage);
+                UE_LOG(LogTemp, Log, TEXT("액세서리 추가: %s"), *RowName.ToString());
+            }
+        }
+    }
+}
--- Source/AGSD/ResultUI.h
+++ Source/AGSD/ResultUI.h
@@ -4,6 +4,12 @@
 
 #include "CoreMinimal.h"
 #include "Blueprint/UserWidget.h"
+#include "Components/Button.h"
+#include "Components/TextBlock.h"
+#include "Components/WrapBox.h"
+#include "Components/Image.h"
+#include "Engine/DataTable.h"
+#include "AccessoryData.h" 
 #include "ResultUI.generated.h"
 
 /**
@@ -15,7 +21,25 @@ class AGSD_API UResultUI : public UUserWidget
 	GENERATED_BODY()
 
 public:
+    virtual void NativeConstruct() override;
+
     // 버튼 위젯 변수
     UPROPERTY(meta = (BindWidget))
     class UButton* CloseResultButton;
+
+    UFUNCTION(BlueprintCallable)
+    void CloseResult();
+
+protected:
+    UPROPERTY(meta = (BindWidget))
+    class UTextBlock* ResultScoreText;
+
+    UPROPERTY(meta = (BindWidget))
+    class UWrapBox* AccessoryWrapBox;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Accessory")
+    UDataTable* AccessoryDataTable;
+
+    void PopulateAccessoryIcons();
+
 };
