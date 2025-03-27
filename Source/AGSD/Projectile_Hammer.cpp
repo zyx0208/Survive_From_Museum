cfe9f7c4fd6e1c4e69ac430344e03914bf64bd23
kiwi <dek1423@naver.com>
2025-03-27 23:12:07 +0900

    해머 구현 완료

---
+++ Source/AGSD/Projectile_Hammer.cpp
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Projectile_Hammer.h"
+#include "GameFramework/Character.h"
+
+AProjectile_Hammer::AProjectile_Hammer()
+{
+    // 투사체 속도, 데미지, 사거리
+    ProjectileSpeed = 0.0f;
+    ProjectileDamage = 4.0f;
+    ProjectileRange = 100.0f;
+}
+
+void AProjectile_Hammer::WeaponHitEffect(AActor* OtherActor)
+{
+    UE_LOG(LogTemp, Display, TEXT("KnockBack"));
+    FVector KnockBackDirection = GetActorForwardVector();
+    ACharacter* HitEnemy = Cast<ACharacter>(OtherActor);
+    if (HitEnemy) {
+        FVector KnockBackVector = KnockBackDirection*3000.0f;
+        HitEnemy->LaunchCharacter(KnockBackVector, true, false);
+    }
+}
+
+void AProjectile_Hammer::Tick(float DeltaTime)
+{
+    Super::Super::Tick(DeltaTime);
+    // 현재 위치와 시작 위치의 거리를 계산
+    float DistanceTraveled = FVector::Dist(StartLocation, GetActorLocation());
+
+    // 특정 거리 이상 이동하면 파괴
+    if (DistanceTraveled > ProjectileRange)
+    {
+        Destroy();
+    }
+}
