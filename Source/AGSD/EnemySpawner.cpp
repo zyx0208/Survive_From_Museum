7f6289b391fe001a7589451c1034cd68020f6f7e
zyx0208 <zyx0208@naver.com>
2025-04-07 01:57:32 +0900

    패치노트 4.7.2
    테스트용으로 썼던 변수 다시 수정
    -보스전 입장 시간을 5초에서 300초로 다시 변경

--- Source/AGSD/EnemySpawner.cpp
+++ Source/AGSD/EnemySpawner.cpp
@@ -41,7 +41,7 @@ void AEnemySpawner::Tick(float DeltaTime)
             TotalTime += DeltaTime;
 
             //보스라운드
-            if (TotalTime >= 5.0f)//이 시간을 바꾸면 보스전 진입 시간이 바뀜(기본 300초)
+            if (TotalTime >= 300.0f)//이 시간을 바꾸면 보스전 진입 시간이 바뀜(기본 300초)
             {
                 if (!BossRound)
                 {
